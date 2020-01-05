1d1c360d655c888096d469cf87201576bc0abe2d
talareq <galuszka.grzegorz@yahoo.com>
2020-01-05 19:33:57 +0100

    Capabilities

--- src/test/java/com/e2e/training/selenium/MyFirstTest.java
+++ src/test/java/com/e2e/training/selenium/MyFirstTest.java
@@ -4,11 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -22,21 +23,22 @@ public class MyFirstTest {
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+    driver = new InternetExplorerDriver();
+    System.out.println(((HasCapabilities) driver).getCapabilities());
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
   public void myFirstTest() {
-    // driver.navigate().to("https://www.google.pl/"); WebElement q = driver.findElement(By.name("q")); q.sendKeys("webdriver");
     driver.get("https://www.google.pl/");
-    driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
-    driver.findElement(By.xpath("//button[@id='K32']"));
-    driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
-    driver.findElement(By.name("q")).sendKeys("webdriver");
-    driver.findElement(By.xpath("//input[@name='btnK']")).click();
-    wait.until(titleIs("webdriver - Szukaj w Google"));
+   // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
+   // driver.findElement(By.xpath("//button[@id='K32']"));
+   // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
+   // driver.findElement(By.name("q")).sendKeys("webdriver");
+   // driver.findElement(By.xpath("//input[@name='btnK']")).click();
+   // wait.until(titleIs("webdriver - Szukaj w Google"));
   }
 
   @After
