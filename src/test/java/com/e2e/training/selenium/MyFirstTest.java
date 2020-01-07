4f897d5e85727ea439939af8eb84cff2f4c49684
talareq <galuszka.grzegorz@yahoo.com>
2020-01-07 11:18:54 +0100

    Cookies

--- src/test/java/com/e2e/training/selenium/MyFirstTest.java
+++ src/test/java/com/e2e/training/selenium/MyFirstTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,6 +17,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.util.Set;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -30,27 +32,31 @@ public class MyFirstTest {
   @Before
   public void start() {
     DesiredCapabilities caps = new DesiredCapabilities();
-    caps.setCapability(FirefoxDriver.MARIONETTE, false); // old browser
-    // caps.setCapability(FirefoxDriver.MARIONETTE, true); // new browser
+    // caps.setCapability(FirefoxDriver.MARIONETTE, false); // old browser
     WebDriver driver = new FirefoxDriver();
     System.out.println(((HasCapabilities) driver).getCapabilities());
     wait = new WebDriverWait(driver, 10);
+    driver.manage().addCookie(new Cookie("test", "test"));
+    Cookie testCookie = driver.manage().getCookieNamed("test");
+    Set<Cookie> cookies = driver.manage().getCookies();
+    driver.manage().deleteCookieNamed("test");
+    driver.manage().deleteAllCookies();
   }
 
   @Test
   public void myFirstTest() {
-//    driver.get("https://www.google.pl/");
-   // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
-   // driver.findElement(By.xpath("//button[@id='K32']"));
-   // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
-   // driver.findElement(By.name("q")).sendKeys("webdriver");
-   // driver.findElement(By.xpath("//input[@name='btnK']")).click();
-   // wait.until(titleIs("webdriver - Szukaj w Google"));
+    // driver.get("https://www.google.pl/");
+    // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
+    // driver.findElement(By.xpath("//button[@id='K32']"));
+    // driver.findElement(By.xpath("//span[@class='hOoLGe']")).click();
+    // driver.findElement(By.name("q")).sendKeys("webdriver");
+    // driver.findElement(By.xpath("//input[@name='btnK']")).click();
+    // wait.until(titleIs("webdriver - Szukaj w Google"));
   }
 
   @After
   public void stop() {
-   driver.quit();
-   driver = null;
+    driver.quit();
+    driver = null;
   }
 }
