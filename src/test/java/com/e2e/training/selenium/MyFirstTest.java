7cb7feb432c13dd7d3c38a25b83bb81cf53f6cd1
talareq <galuszka.grzegorz@yahoo.com>
2020-01-21 12:27:43 +0100

    Browser_log

--- src/test/java/com/e2e/training/selenium/MyFirstTest.java
+++ src/test/java/com/e2e/training/selenium/MyFirstTest.java
@@ -2,19 +2,27 @@ package com.e2e.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
  * Created by m on 2020-01-03.
  */
 public class MyFirstTest extends TestBase {
+
+  @Test
+  public void getBrowserLogs() {
+    driver.navigate().to("https://www.google.pl/");
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    //System.out.println(driver.manage().logs().getAvailableLogTypes());
+    //driver.manage().logs().get("performance").forEach(l -> System.out.println(l));
+    for (LogEntry l : driver.manage().logs().get("browser").getAll()) {
+       System.out.println(l);
+     }
+
+  }
 
   @Test
   public void myFirstTest() {
